5169abdcfc086bd411bce8c68c6ed1b5bc611c75
Emmanuel Onwuka <onwukamanuel@gmail.com>
2016-12-29 22:48:19 -0500

    Create main.m

---
+++ main.m
@@ -0,0 +1,33 @@
+%  04/07/16
+
+%% Problem 1a
+clc
+figure(1)
+syms f(t) t
+f(t) = ((3*t+3).*heaviside(t+2) - 6*t.*heaviside(t)).*heaviside(-t+2);
+grid on;
+ezplot(f, [-4,4])
+hold off
+
+x = -10:10;
+[C,~] = polarctfs(f,4,10);
+figure(3)
+hold on
+plotyy(x,abs(C),x,angle(C),'stem','stem')
+hold off
+%% %Problem 1b
+clc
+syms t x(t)
+x(t) = heaviside(t+.5).*heaviside(-t+.5);
+figure
+grid on
+hold on
+ezplot(x, [-2.5,2.5])
+
+q = -15:15;
+[C,w] = polarctfs(x,5,15);
+figure(3)
+hold on
+plotyy(q,abs(C),q,angle(C),'stem','stem')
+legend('abs(C)','angle(C)')
+hold off
